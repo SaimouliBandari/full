3ac63b54c3df03c437b42096018f9458b51a9d26
SaimouliBandari <saimouli4k@gmail.com>
2022-08-13 23:54:15 +0530

    commit 1

--- index.js
+++ index.js
@@ -1,65 +1,85 @@
-"use strict";
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const { request } = require('express');
+const mongoose = require('mongoose');
+const urlExists = require('node:url');
+const dns = require('dns');
 
-var express = require("express");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var cors = require("cors");
-var dns = require("dns");
-var app = express();
-var router = express.Router;
 // Basic Configuration
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("we're connected!");
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true, useUnifiedTopology: true}, (err) => {
+  if(err) console.log(err);
+  else
+    console.log("connected to database");
 });
 
-//Schema n Model
-var urlSchema = new mongoose.Schema({
-  id: Number,
-  url: String
+const UrlSchema = new mongoose.Schema({
+  original : {type : String, required: true},
+  short : Number
 });
 
-var urlModel = mongoose.model("urlModel", urlSchema);
+let shortUrl = mongoose.model('shortUrl', UrlSchema);
+
 
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-//mycode
-app.post("/api/shorturl/new", function(req, res) {
+//let responseObject= {};
+// app.post("/api/shorturl",bodyParser.urlencoded({extended: false}) ,(req, res) => {
+//   let inputUrl = req.body['url'];
+  
+//     if(inputUrl === ""){
+//       responseObject["error"] = "Invalid URL";
+//       res.send(responseObject);
+//     }
+//     urlExists(inputUrl,  (err, exists) => {
+//       if(exists){
+//         responseObject["original_url"] = inputUrl;
+//       }else{
+//         responseObject["error"] = "Invalid URL";
+//       }
+//     });
+
+    
+
+
+
+
+
+
+
+  
+// });
+
+
+app.post("/api/shorturl", function(req, res) {
   let urlRegex = /https:\/\/www.|http:\/\/www./g;
   
   dns.lookup(req.body.url.replace(urlRegex, ""), (err, address, family) => {
     if (err) {
       res.json({"error":"invalid URL"});
     } else {
-      urlModel
+      shortUrl
         .find()
         .exec()
         .then(data => {
-          new urlModel({
-            id: data.length + 1,
-            url: req.body.url
+          new shortUrl({
+            short: data.length + 1,
+            original: req.body.url
           })
             .save()
             .then(() => {
@@ -76,16 +96,17 @@ app.post("/api/shorturl/new", function(req, res) {
   });
 });
 
-//get
 app.get("/api/shorturl/:number", function(req, res) {
-  urlModel
-    .find({ id: req.params.number })
-    .exec()
-    .then(url => {
-      res.redirect(url[0]["url"]);
-    });
+  let input = req.params.number;
+   shortUrl.findOne({short : input}, (err, result) => {
+      if(!err && result != undefined){
+          res.redirect(result.original);
+      }
+   })
 });
 
 app.listen(port, function() {
-  console.log("Node.js listening ...");
+  console.log(`Listening on port ${port}`);
 });
+
+
